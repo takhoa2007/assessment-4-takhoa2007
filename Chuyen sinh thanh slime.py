163545d0ccbc20c224f2d3a949ed0d872b94af0a
takhoa2007 <a1989330@adelaide.edu.au>
2026-05-19 20:49:57 +0930

    test 1

---
+++ Chuyen sinh thanh slime.py
@@ -0,0 +1,10 @@
+"""
+Filename: Chuyen sinh thanh slime.py
+Description: 
+Author: Anh Khoa Truong
+AU Username: a1989330
+GitHub Classroom Username: takhoa2007
+
+This is my own work as defined by the Adelaide University's Academic Misconduct Policy.
+"""
+print("Hello")
--- slime.py
+++
@@ -1,9 +0,0 @@
-"""
-Filename: slime.py
-Description: 
-Author: 
-AU Username: 
-GitHub Classroom Username: 
-
-This is my own work as defined by the Adelaide University's Academic Misconduct Policy.
-"""
