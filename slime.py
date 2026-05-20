19f0fc5b8f3b64f3f0d24b87ba1178cc5fc73389
takhoa2007 <a1989330@adelaide.edu.au>
2026-05-20 20:36:26 +0930

    Getter and Setter is added. In setter, the type of parameter is checked, if its not correct, raise the TypeError and ValueError.

--- slime.py
+++ slime.py
@@ -14,9 +14,10 @@ import math
 import random
 
 class Slime(ABC):
+    #Initial value (Could not be changed)
     __id_counter = 0
     INITIAL_POWER = 5.0
-    def __init__(self,ID,name,size):
+    def __init__(self,ID,name,size) -> None:
         Slime.__id_counter += 1
         self.__id = f"SLM-{Slime._id_counter}"
         self.__name = name
@@ -26,4 +27,24 @@ class Slime(ABC):
         self.__volatility_level = random.randint(0,10)
         self.__power = self.INITIAL_POWER
 
+    def get_id(self) -> str:
+        return self.__id
+    
+    def get_name(self) -> str:
+        return self.__name
+    
+    def set_name(self,name) -> None:
+        if not isinstance(name,str):
+            raise TypeError("Name must be a string.")
+        if not name.strip():
+            raise ValueError("Name must not be empty.")
+        self.__name = name
 
+    def get_size(self) -> float:
+        return self.__size
+    
+    def set_size(self,size) -> None:
+        if not isinstance(size, (int,float)):
+            raise TypeError("Must be a numeric value.")
+        # Raises TypeError: If value is not a numeric type (int or float).
+        self.__size = float(size)
